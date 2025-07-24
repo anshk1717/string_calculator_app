ac84d7a604a047e93902a9d940290132e9f121ef
ashish <anshk1717@gmail.com>
2025-07-24 21:10:11 +0530

    refactored code

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,22 +1,21 @@
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    return numbers.to_i if numbers.to_i.to_s == numbers
-    numbers.split(',').map(&:to_i).reduce(:+)
-    numbers.split(/[\n,]/).map(&:to_i).reduce(:+)
+
     if numbers.start_with?("//")
       delimiter, numbers = numbers[2..].split("\n", 2)
+      delimiter = Regexp.escape(delimiter)
     else
       delimiter = /[\n,]/
     end
-    numbers.split(delimiter).map(&:to_i).reduce(:+)
-    nums = numbers.split(delimiter).map(&:to_i)
+
+    nums = numbers.split(Regexp.new(delimiter)).map(&:to_i)
     negatives = nums.select { |n| n < 0 }
 
     unless negatives.empty?
       raise "negative numbers not allowed: #{negatives.join(', ')}"
     end
 
-    nums.reduce(:+)
+    nums.sum
   end
 end
