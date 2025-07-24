0beed4e26696a7e7ee3b62db3d1ed913b7d979c7
ashish <anshk1717@gmail.com>
2025-07-24 20:53:06 +0530

    test case added for new line

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -19,5 +19,10 @@ RSpec.describe StringCalculator do
         expect(StringCalculator.add("1,2")).to eq(3)
       end
     end
+    context "when the input contains new lines" do
+      it "returns the sum of the numbers" do
+        expect(StringCalculator.add("1\n2,3")).to eq(6)
+      end
+    end
   end
 end
